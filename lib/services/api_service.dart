c01bef417249af277ec5102aacb14a6e1498947e
yih1612 <yih1612@naver.com>
2023-12-05 10:34:08 +0900

    6.5 wait for webtoons

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,10 +4,11 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
@@ -16,6 +17,7 @@ class ApiService {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
         webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        print(webtoon);
       }
       return webtoonInstances;
     }
