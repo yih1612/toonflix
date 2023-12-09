feb39c5b108603d791d5a69a13c534f40187e5b8
yih1612 <yih1612@naver.com>
2023-12-09 11:39:34 +0900

    6.14 detail info

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -76,6 +76,38 @@ class _DetailScreenState extends State<DetailScreen> {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 20,
+            ),
+            FutureBuilder(
+                future: webtoon,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 50,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            snapshot.data!.about,
+                            style: const TextStyle(fontSize: 16),
+                          ),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w600),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                  return const Text('...');
+                })
           ],
         ));
   }
